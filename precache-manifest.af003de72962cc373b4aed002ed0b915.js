fab152c8a7ccdeefdd83dfc6ff5fc8ff11a4ecc8
Tetiana Yasko <taniayasko2015@gmail.com>
2021-08-17 12:56:36 +0300

    Updates

---
+++ precache-manifest.af003de72962cc373b4aed002ed0b915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5611820c24d84f570c00edb2d8c1a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9fa2446482e0cca1820a",
+    "url": "./static/css/main.bb9ead99.chunk.css"
+  },
+  {
+    "revision": "c9c02cf07ac53a3ca84c",
+    "url": "./static/js/2.e9a53571.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e9a53571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa2446482e0cca1820a",
+    "url": "./static/js/main.9f418042.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
